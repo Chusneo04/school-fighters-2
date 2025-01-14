d5a582c7dae711540077ac928df502a48ee2e6df
Hugo Arroyo <hugoarroyogalle@gmail.com>
2025-01-14 12:52:37 +0100

    kvvbn

--- src/views/VistaPersonajes.vue
+++ src/views/VistaPersonajes.vue
@@ -1,6 +1,15 @@
 <template>
-    <section>
-  
+    <section class="landscape:flex landscape:flex-col landscape:justify-center landscape:items-center landscape:bg-[url('/src/assets/img/fondo.jpg')]">
+        <h1 class="text-[40px] font-bold ">SCHOOL FIGHTER 2</h1>
+        <p class="">El mejor juego de lucha entre alumnos. Selecciona los alumnos que van a darse de hostias</p>
+        <div class="">
+            <select>
+                <option value="">Ana Busado de Hesa</option>
+            </select>
+            <select>
+                <option value="">Cindy Entes</option>
+            </select>
+        </div>
     </section>
   </template>
   
