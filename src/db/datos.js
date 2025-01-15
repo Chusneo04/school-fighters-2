ba3063352453163d4833f6199346fefe5ea8a3c0
Hugo Arroyo <hugoarroyogalle@gmail.com>
2025-01-15 09:57:52 +0100

    Hacemos que el escenario seleccionado sea el arena de combate

--- src/db/datos.js
+++ src/db/datos.js
@@ -1,14 +1,14 @@
 export let personajes = [
-    {nombre: 'Alan Brito Delgado', imagen :'/src/assets/img/1.jpg'},
-    {nombre: 'Kelyn Teresa Austed', imagen :'/src/assets/img/2.jpg'},
-    {nombre: 'Rosa Amel Fierro', imagen :'/src/assets/img/3.jpg'},
-    {nombre: 'Ema Amado Bustos', imagen :'/src/assets/img/4.jpg'},
-    {nombre: 'Mikoko Yanosuda', imagen :'/src/assets/img/5.jpg'}
+    {nombre: 'Alan Brito Delgado', imagen :'./src/assets/img/1.jpg'},
+    {nombre: 'Kelyn Teresa Austed', imagen :'./src/assets/img/2.jpg'},
+    {nombre: 'Rosa Amel Fierro', imagen :'./src/assets/img/3.jpg'},
+    {nombre: 'Ema Amado Bustos', imagen :'./src/assets/img/4.jpg'},
+    {nombre: 'Mikoko Yanosuda', imagen :'./src/assets/img/5.jpg'}
     
 ]
 
 export let escenarios = [
-    {nombre: 'Sandy Shores', imagen:'/src/assets/img/SandyShores.jpg'},
-    {nombre: 'Strawberry', imagen:'/src/assets/img/StrawberryAvenue.jpg'},
-    {nombre:'Vespucci', imagen: '/src/assets/img/VespucciBeach.jpg'}
+    {nombre: 'Grove Street', imagen:'https://preview.redd.it/oaemijngj0kc1.jpeg?auto=webp&s=fcae643fd4cf5243f4caaab229782a3c2151c797'},
+    {nombre: 'Casa de Michael', imagen:'https://i.ytimg.com/vi/tFOMeyI869k/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDlP-Pt4SRFPp8wmKVkKttHrebdrw'},
+    {nombre:'Casa de Franklin', imagen: 'https://www.gtainside.com/downloads/picr/2018-01/1517138811_enb%202018_01_27%2020_46_57_42.jpg'}
 ]
--- src/views/VistaCombate.vue
+++ src/views/VistaCombate.vue
@@ -1,11 +1,28 @@
 <template>
-    <section class="h-screen bg-no-repeat bg-cover landscape:flex landscape:flex-col p-[20px] gap-[20px]">
+    <section :style="{ backgroundImage: `url(${this.escenario.imagen})`}" class="h-screen bg-no-repeat bg-cover landscape:flex landscape:flex-col p-[20px] gap-[20px]">
         <router-link to="/"><button class="bg-[#1e8fde] h-[40px] w-[100px] rounded-[7px] font-bold text-[24px] text-[white]">Volver</button></router-link>
     </section>
   </template>
   
   <script>
   export default {
-      name: 'VistaCombate'
+      name: 'VistaCombate',
+      data(){
+        return{
+            escenario: ''
+        }
+        
+      },
+      methods:{
+        guardarEscenario(){
+            this.escenario = JSON.parse(localStorage.getItem('escenario'))
+        }
+      },
+      mounted(){
+        this.guardarEscenario()
+        console.log(this.escenario);
+        
+      }
+      
   }
   </script>
